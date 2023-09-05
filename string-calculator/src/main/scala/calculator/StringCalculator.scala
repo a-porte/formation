56da7a80f0c406594eea17a63b2722ed426456d9
a-porte <104493676+a-porte@users.noreply.github.com>
2023-09-05 16:39:37 +0200

    Added a function literal

--- string-calculator/src/main/scala/calculator/StringCalculator.scala
+++ string-calculator/src/main/scala/calculator/StringCalculator.scala
@@ -2,20 +2,21 @@ package calculator
 
 class StringCalculator :
   private val isNegative :Int => Boolean = (i:Int) => i < 0
+  private val customDelimitor : String => Boolean = _.contains("//")
   def add(input: String = ""): String =
     input match
       case "" => "0"
       case line =>
         val delim: String = parseDelim(line)
         val intList =  line.split(s"[,|\n|$delim]").toList.filter(_.nonEmpty)
-          .filterNot(_.contains("//")).map(_.toInt).filter(_ < 1000)
+          .filterNot(customDelimitor).map(_.toInt).filter(_ < 1000)
 
         intList.count(isNegative) match
           case 0 => intList.sum.toString
           case _ => raiseCustomException(intList.filter(isNegative))
 
   private def parseDelim(line: String) =
-     line.split("\n").toList.filter(_.contains("//"))
+     line.split("\n").toList.filter(customDelimitor)
        .map(_.mkString).mkString //flattening everything
        .replace("//", "")
 
