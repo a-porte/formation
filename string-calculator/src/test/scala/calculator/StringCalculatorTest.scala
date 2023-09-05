753378253ed8a3a298779830190650bb94510925
a-porte <104493676+a-porte@users.noreply.github.com>
2023-09-05 11:56:44 +0200

    Some refacto

--- string-calculator/src/test/scala/calculator/StringCalculatorTest.scala
+++ string-calculator/src/test/scala/calculator/StringCalculatorTest.scala
@@ -22,19 +22,18 @@ class StringCalculatorTest extends AnyFlatSpec :
     assert(StringCalculator().add("//;\n1,2;3") == "6")
 
   "one negative number " should "raise an exception " in {
-    val except = intercept[Exception] {
+    val except = intercept[NumberFormatException] {
       StringCalculator().add("-1,2,3")
     }
     assert(except.getMessage == "Les nombres négatifs ne sont pas autorisés")
   }
 
   "several negative numbers " should "raise a different exception message " in {
-    val except = intercept[Exception] {
+    val except = intercept[NumberFormatException] {
     StringCalculator().add("-5,2,-10,9")
     }
     assert(except.getMessage == "Les nombres négatifs ne sont pas autorisés : -5, -10")
-}
-
+  }
 
   "number > 1 000 " should " be ignored" in
     assert(StringCalculator().add("5,10,1664") == "15")
