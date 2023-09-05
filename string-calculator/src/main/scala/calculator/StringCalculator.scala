94aa3040eaac1dac2dfaffd00f805c2a9e1cf69f
a-porte <104493676+a-porte@users.noreply.github.com>
2023-09-05 11:27:02 +0200

    Some refacto : one lambda used to identify negative numbers

--- string-calculator/src/main/scala/calculator/StringCalculator.scala
+++ string-calculator/src/main/scala/calculator/StringCalculator.scala
@@ -1,14 +1,15 @@
 package calculator
 
 class StringCalculator :
+  private val isNegative :Int => Boolean = (i:Int) => i < 0
   def add(input: String = ""): String =
     input match
       case "" => "0"
       case line =>
         val intList = line.split(s"[^0-9-]").toList.filter(_.nonEmpty).map(_.toInt).filter(_ < 1000)
-        intList.count(_ < 0) match
+        intList.count(isNegative) match
           case 0 => intList.sum.toString
-          case _ => raiseCustomException(intList.filter(_ < 0))
+          case _ => raiseCustomException(intList.filter(isNegative))
 
   private def raiseCustomException(negList: List[Int]) =
     throw
