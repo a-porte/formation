02fbb4ba24633151830e3b0db5e614c7a78f89dd
a-porte <104493676+a-porte@users.noreply.github.com>
2023-09-05 11:21:23 +0200

    Some refacto

--- string-calculator/src/main/scala/calculator/StringCalculator.scala
+++ string-calculator/src/main/scala/calculator/StringCalculator.scala
@@ -8,6 +8,9 @@ class StringCalculator :
         val intList = line.split(s"[^0-9-]").toList.filter(_.nonEmpty).map(_.toInt)
         intList.count(_ < 0) match
           case 0 => intList.sum.toString
-          case 1 => throw new NumberFormatException("Les nombres négatifs ne sont pas autorisés")
-          case 2 => throw new NumberFormatException(s"Les nombres négatifs ne sont pas autorisés : ${intList.filter(_ < 0).mkString(", ")}")
+          case _ => raiseCustomException(intList.filter(_ < 0))
 
+  private def raiseCustomException(negList: List[Int]) =
+    throw
+      val innerInterpolatedString = if negList.size > 1 then " : " + negList.mkString(", ") else ""
+      new NumberFormatException(s"Les nombres négatifs ne sont pas autorisés$innerInterpolatedString")
