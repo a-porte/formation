18875c723a8bfe2858d26ebb7bed96b3b7e375cd
a-porte <104493676+a-porte@users.noreply.github.com>
2023-09-22 11:50:00 +0200

    main.tf formatted + tag added

--- learn-terraform-azure/main.tf
+++ learn-terraform-azure/main.tf
@@ -28,13 +28,18 @@ newzealandnorth,southeastasiafoundational,spaincentral,taiwannorth,taiwannorthwe
 resource "azurerm_resource_group" "rg" {
   name     = "myTFResourceGroup"
   location = "francecentral"
+
+  tags = {
+    Environment = "Terraform Getting Started"
+    Team        = "DevOps"
+  }
 }
 
 resource "azurerm_virtual_network" "vnet" {
-	name = "myTFVnet"
-	address_space =  ["10.0.0.0/16"]
-	location = "francecentral"
-	resource_group_name = azurerm_resource_group.rg.name
+  name                = "myTFVnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = "francecentral"
+  resource_group_name = azurerm_resource_group.rg.name
 }
 
 
