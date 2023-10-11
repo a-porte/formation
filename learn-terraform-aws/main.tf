814697532f7ce54a33e401d1b92de01ed48b1db0
a-porte <104493676+a-porte@users.noreply.github.com>
2023-10-11 16:48:03 +0200

    Tried some stuff with the provisioners remote_exec and file ones require creation and declaration of key pair + a connection definition

--- learn-terraform-aws/main.tf
+++ learn-terraform-aws/main.tf
@@ -3,12 +3,28 @@ provider "aws" {
   access_key = var.SECRET.access_key
   secret_key = var.SECRET["secret_key"] #both way to access a map element
 }
+# Have not been able send key to EC2 because of a lack of authorisations
+resource "aws_key_pair" "key_pair" {
+  public_key = file("./terraform.pub")
+  key_name = "terraform-key"
+}
 
 resource "aws_instance" "my_ec2_instance" {
+  timeouts { # appears to do nothing at all if a remote_exec is ongoing
+    create = "50s"
+  }
+  key_name = aws_key_pair.key_pair.key_name
   ami = var.AWS_AMIS[var.AWS_REGION] #change AMI according to region name
   instance_type = "t2.nano"
   tags = {
     Name = "test_ec2_with_terraform"
+  }
+
+  connection {
+    type = "ssh"
+    user = "ubuntu"
+    private_key = file("./terraform")
+    host = self.public_ip
   }
 
   user_data = <<-EOF
@@ -24,6 +40,29 @@ resource "aws_instance" "my_ec2_instance" {
   vpc_security_group_ids = [aws_security_group.secu_grp.id]
   # creates a dependency
   # <provider>_<type>.<resource_name>.<field_type>
+
+  # provisioners should be used in last resort
+  #https://developer.hashicorp.com/terraform/language/resources/provisioners/syntax
+  provisioner "local-exec" { # realizes a defined command on the machine where and when apply is executed
+    command = "echo ${aws_instance.my_ec2_instance.public_ip} > ip_address.txt"
+  }
+
+  provisioner "local-exec" {
+    when = destroy
+    command = "rm -v ip_address.txt"
+  }
+
+#  provisioner "file" {
+  # copies from <source> on the local machine to <destination> on the remote one
+#    source = "./to_copy.txt"
+#    destination = "/tmp"
+#  }
+
+ #requires a connection
+  # execute commands on the remote
+#  provisioner "remote-exec" {
+#    inline = ["ls -lat /tmp"]
+#  }
 }
 
 resource "aws_security_group" "secu_grp" {
@@ -42,6 +81,15 @@ resource "aws_security_group" "secu_grp" {
     to_port = 80
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  #required to send file via SSH
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+
   }
 
 #  egress {
